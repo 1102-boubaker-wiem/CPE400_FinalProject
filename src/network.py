7954b6654a5c7963781ebe38f15b34baefbce87a
jaketheduque <jxie7488@gmail.com>
2025-11-28 19:40:03 -0800

    no longer using broadcast_ogm function

--- src/network.py
+++ src/network.py
@@ -6,7 +6,18 @@ class Network:
         
     def broadcast_ogm(self):
         for node in self.nodes:
-            node.broadcast_ogm()
+            ogm_packet = Packet(
+                type = "ogm",
+                src = node.identifier,
+                dest = "FF:FF:FF:FF:FF:FF",
+                data = {
+                    "originator": node.identifier,
+                    "sequence": node.sequence_number,
+                    "ttl": 500
+                }
+            )
+            node.sequence_number += 1
+            node.send_packet(ogm_packet)
     
     def get_node(self, identifier):
         return [node for node in self.nodes if node.identifier == identifier][0]
