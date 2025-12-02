e3a4094522e0e1539e4c30376259a9c31bbcf7ff
1102-boubaker-wiem <boubakerwiem@gmail.com>
2025-12-01 20:17:31 -0800

    add size to packet size

--- src/packet.py
+++ src/packet.py
@@ -1,10 +1,11 @@
 class Packet:   
-    def __init__(self, type, src, dest, data):
+    def __init__(self, type, src, dest, data,size):
         self.type = type
         self.src = src
         self.dest = dest
         self.data = data
         self.path = [src]
+        self.size = size                # packet size in bytes (default = 1024 bytes)
         
     def __str__(self):
-        return f"Packet(type={self.type!r}, src={self.src}, dest={self.dest}, data={self.data!r}, path={self.path})"
+        return f"Packet(type={self.type!r}, src={self.src}, dest={self.dest}, data={self.data!r}, size={self.size} bytes, path={self.path})"
