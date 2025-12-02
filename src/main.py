178e7eb19dfceb013300895aac3aaf6c2b69d655
jaketheduque <jxie7488@gmail.com>
2025-12-01 19:26:34 -0800

    iterate through multiple link_probs and save results in stats.csv

--- src/main.py
+++ src/main.py
@@ -1,56 +1,66 @@
 from graph_generator import generate_graph, mutate_graph
-from path_finder import find_path
-from flow_simulator import FlowSimulator
 from packet import Packet
 from network import Network
 import random
-
+import csv
 
 def main():
     # 1. Generate random network
     network = Network()
-    nodes = generate_graph(network, 25)
     
-    network.broadcast_ogm()
-    
+    stats = []
     num_packets = 100
-    travel_time_sum = 0
-    packets_lost = 0
-    for i in range(0, num_packets):
-        mutate_graph(nodes) 
-        network.broadcast_ogm()
-        # reset routing state
-        for node in nodes:
-            node.routing_table = {}
-            node.ogm_counts = {}
-            node.ogm_sequence_nums = {}
+    for link_prob in [x / 100.0 for x in range(5, 100, 5)]:
+        travel_time_sum = 0
+        packets_lost = 0
+        
+        nodes = generate_graph(network, 25, link_prob)
+        network.nodes = nodes
+        
+        # # reset routing state
+        # for node in nodes:
+        #     node.routing_table = {}
+        #     node.ogm_counts = {}
+        #     node.ogm_sequence_nums = {}
         # rebuild routing tables
         for _ in range(4):   # 4 floods makes routing converge
             network.broadcast_ogm()
-
-        src_node = random.choice([n for n in nodes])
-        dest_node = random.choice([n for n in nodes if n is not src_node])
-        packet = Packet(
-            "data",
-            src_node.identifier,
-            dest_node.identifier,
-            {}
-        )
         
-        travel_time = src_node.send_packet(packet)
-        if travel_time < 0:
-            print("\033[1;31mPacket Lost:\033[22m No route could be found!\033[0m")
-            packets_lost += 1
-        else:
-            print(f"\033[1;32mPacket Travel Time:\033[22m {travel_time}\033[0m")
-            travel_time_sum += travel_time
-                        
-    print()
-    print(f"\033[1mPackets Lost: {packets_lost} ({(packets_lost/num_packets)*100}%)\033[0m")
-    
-    if (packets_lost != num_packets):
-        average_travel_time = travel_time_sum / (num_packets - packets_lost)
-        print(f"\033[1mAverage Travel Time: {average_travel_time}\033[0m")
+        for _ in range(0, num_packets):
+            src_node = random.choice([n for n in nodes])
+            dest_node = random.choice([n for n in nodes if n is not src_node])
+            packet = Packet(
+                "data",
+                src_node.identifier,
+                dest_node.identifier,
+                {}
+            )
+            
+            travel_time = src_node.send_packet(packet)
+            if travel_time < 0:
+                # print("\033[1;31mPacket Lost:\033[22m No route could be found!\033[0m")
+                packets_lost += 1
+            else:
+                # print(f"\033[1;32mPacket Travel Time:\033[22m {travel_time}\033[0m")
+                travel_time_sum += travel_time
+            
+        mutate_graph(nodes, link_prob)       
+        print()
+        print(f"\033[1mPackets Lost w/ {link_prob} link prob: {packets_lost} ({(packets_lost/num_packets)*100}%)\033[0m")
+        
+        average_travel_time = -1
+        if (packets_lost != num_packets):
+            average_travel_time = travel_time_sum / (num_packets - packets_lost)
+            print(f"\033[1mAverage Travel Time w/ {link_prob} link prob: {average_travel_time}\033[0m")
+            
+        stats.append({link_prob, packets_lost, average_travel_time})
+        
+    with open("stats.csv", "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow({"link_prob", "packets_lost", "average_travel_time"})
+        writer.writerows(stats)
+
+    print("Data written to stats.csv successfully.")
 
 if __name__ == "__main__":
     main()
